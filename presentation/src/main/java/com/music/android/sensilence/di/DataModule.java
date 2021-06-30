4f65d34333699fcf45ece575ed2f6dd91f51337c
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-06-30 08:32:48 +0300

    update version

--- presentation/src/main/java/com/music/android/sensilence/di/DataModule.java
+++ presentation/src/main/java/com/music/android/sensilence/di/DataModule.java
@@ -4,10 +4,7 @@ import dagger.Module;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
 
-@Module(includes = {
-        RepositoryModule.class,
-        UseCaseModule.class
-})
+@Module(includes = {RepositoryModule.class, UseCaseModule.class})
 @InstallIn(SingletonComponent.class)
 public class DataModule {
 }
