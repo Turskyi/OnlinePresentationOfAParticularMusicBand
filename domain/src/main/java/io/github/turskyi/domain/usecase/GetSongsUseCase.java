dbca6c81ffe808cc9e8bbe7a5641bcb72c6e9941
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2022-05-05 17:34:13 -0400

    update May 2022

--- domain/src/main/java/io/github/turskyi/domain/usecase/GetSongsUseCase.java
+++ domain/src/main/java/io/github/turskyi/domain/usecase/GetSongsUseCase.java
@@ -5,7 +5,6 @@ import java.util.List;
 import io.github.turskyi.domain.entities.pojo.Song;
 
 public interface GetSongsUseCase {
-
     io.reactivex.rxjava3.disposables.Disposable setSongs(
             String album,
             io.reactivex.rxjava3.functions.Consumer<List<Song>> successConsumer,
